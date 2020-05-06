e662319f980c6ad93b9dd9594c6b7dd27b4be9dd
hho114 <hho114@csu.fullerton.edu>
2020-05-05 21:54:46 -0700

    update run for ios, need to test on ubuntu

--- functions.h
+++ functions.h
@@ -1,110 +1,110 @@
-#ifndef FUNCTION
-#define FUNCTION
-// Calculate distance between point and centroid using the euclidean distance
-float distanceBetween(const float *point, const float *centroid, const int dimension)
-{
-    float dist = 0.0;
-    for (int i = 0; i < dimension; i++)
-    {
-        float diff = point[i] - centroid[i];
-        dist += diff * diff;
-    }
-    return dist;
-}
+// #ifndef FUNCTION
+// #define FUNCTION
+// // Calculate distance between point and centroid using the euclidean distance
+// float distanceBetween(const float *point, const float *centroid, const int dimension)
+// {
+//     float dist = 0.0;
+//     for (int i = 0; i < dimension; i++)
+//     {
+//         float diff = point[i] - centroid[i];
+//         dist += diff * diff;
+//     }
+//     return dist;
+// }
 
-// Assign label to the correct cluster by computing its distances to each cluster centroid.
-int assignLabel(const float *points, float *centroids,
-                const int k, const int dimension)
-{
-    int bestCluster = 0;
-    float bestDist = distanceBetween(points, centroids, dimension); //calculate first point and first centroid distance
-    float *centroid = centroids + dimension;                        //skip first centroid
-    for (int c = 1; c < k; c++, centroid += dimension)
-    {
-        float dist = distanceBetween(points, centroid, dimension);
-        if (dist < bestDist)
-        {
-            bestCluster = c;
-            bestDist = dist;
-        }
-    }
-    return bestCluster;
-}
+// // Assign label to the correct cluster by computing its distances to each cluster centroid.
+// int assignLabel(const float *points, float *centroids,
+//                 const int k, const int dimension)
+// {
+//     int bestCluster = 0;
+//     float bestDist = distanceBetween(points, centroids, dimension); //calculate first point and first centroid distance
+//     float *centroid = centroids + dimension;                        //skip first centroid
+//     for (int c = 1; c < k; c++, centroid += dimension)
+//     {
+//         float dist = distanceBetween(points, centroid, dimension);
+//         if (dist < bestDist)
+//         {
+//             bestCluster = c;
+//             bestDist = dist;
+//         }
+//     }
+//     return bestCluster;
+// }
 
-// Add points into a sum of points.
-void addPoint(const float *points, float *sums, const int dimension)
-{
-    for (int i = 0; i < dimension; i++)
-    {
-        sums[i] += points[i];
-    }
-}
+// // Add points into a sum of points.
+// void addPoint(const float *points, float *sums, const int dimension)
+// {
+//     for (int i = 0; i < dimension; i++)
+//     {
+//         sums[i] += points[i];
+//     }
+// }
 
-// Print the centroids one per line.
-void notifyUpdateCentroids(float *centroids, const int k, const int dimension, int *counter)
-{
-    FILE *fpo;
-    if (*counter == 1)
-    {
-        fpo = fopen("./data/old_centroids.dat", "w");
-    }
-    else
-    {
+// // Print the centroids one per line.
+// void notifyUpdateCentroids(float *centroids, const int k, const int dimension, int *counter)
+// {
+//     FILE *fpo;
+//     if (*counter == 1)
+//     {
+//         fpo = fopen("./data/old_centroids.dat", "w");
+//     }
+//     else
+//     {
 
-        fpo = fopen("./data/old_centroids.dat", "a");
-    }
+//         fpo = fopen("./data/old_centroids.dat", "a");
+//     }
 
-    float *p = centroids;
+//     float *p = centroids;
 
-    for (int i = 0; i < k; i++)
-    {
-        printf("Update centroids: ");
-        for (int j = 0; j < dimension; j++, p++)
-        {
-            printf("%f ", *p);
-            fprintf(fpo, "%f\t", *p);
-        }
-        fprintf(fpo, "\n");
-        printf("\n");
-    }
+//     for (int i = 0; i < k; i++)
+//     {
+//         printf("Update centroids: ");
+//         for (int j = 0; j < dimension; j++, p++)
+//         {
+//             printf("%f ", *p);
+//             fprintf(fpo, "%f\t", *p);
+//         }
+//         fprintf(fpo, "\n");
+//         printf("\n");
+//     }
 
-    fclose(fpo);
+//     fclose(fpo);
 
-    *counter = *counter + 1;
-}
+//     *counter = *counter + 1;
+// }
 
-void inittialCentroids(float *centroids, const int k, const int dimension)
-{
-    FILE *fpo = fopen("./data/init_centroids.dat", "w");
+// void inittialCentroids(float *centroids, const int k, const int dimension)
+// {
+//     FILE *fpo = fopen("./data/init_centroids.dat", "w");
 
-    float *p = centroids;
+//     float *p = centroids;
 
-    for (int i = 0; i < k; i++)
-    {
-        printf("Initital centroids: ");
-        for (int j = 0; j < dimension; j++, p++)
-        {
-            printf("%f ", *p);
-            fprintf(fpo, "%f\t", *p);
-        }
-        // fprintf(fpo, "\n");
-        printf("\n");
-    }
+//     for (int i = 0; i < k; i++)
+//     {
+//         printf("Initital centroids: ");
+//         for (int j = 0; j < dimension; j++, p++)
+//         {
+//             printf("%f ", *p);
+//             fprintf(fpo, "%f\t", *p);
+//         }
+//         // fprintf(fpo, "\n");
+//         printf("\n");
+//     }
 
-    fclose(fpo);
-}
+//     fclose(fpo);
+// }
 
-// Creates an array of random floats. Each number has a value from 0 - 1
-float *createRandomNums(const int totalPoints)
-{
-    float *rand_nums = (float *)malloc(sizeof(float) * totalPoints);
-    rand_nums != NULL;
-    for (int i = 0; i < totalPoints; i++)
-    {
-        rand_nums[i] = (rand() / (float)RAND_MAX);
+// // Creates an array of random floats. Each number has a value from 0 - 1
+// float *createRandomNums(const int totalPoints)
+// {
+//     float *rand_nums = (float *)malloc(sizeof(float) * totalPoints);
+//     rand_nums != NULL;
+//     for (int i = 0; i < totalPoints; i++)
+//     {
+//         rand_nums[i] = (rand() / (float)RAND_MAX);
 
-    }
-    return rand_nums;
-}
+//     }
+//     return rand_nums;
+// }
 
-#endif
+// #endif
--- main.c
+++ main.c
@@ -1,9 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi/mpi.h>
+#include <mpi.h>
+#include <time.h> 
 #include <unistd.h>
 #include <string.h>
-#include "functions.h"
+// #include "functions.h"
+// Calculate distance between point and centroid using the euclidean distance
+float distanceBetween(const float *point, const float *centroid, const int dimension)
+{
+    float dist = 0.0;
+    for (int i = 0; i < dimension; i++)
+    {
+        float diff = point[i] - centroid[i];
+        dist += diff * diff;
+    }
+    return dist;
+}
+
+// Assign label to the correct cluster by computing its distances to each cluster centroid.
+int assignLabel(const float *points, float *centroids,
+                const int k, const int dimension)
+{
+    int bestCluster = 0;
+    float bestDist = distanceBetween(points, centroids, dimension); //calculate first point and first centroid distance
+    float *centroid = centroids + dimension;                        //skip first centroid
+    for (int c = 1; c < k; c++, centroid += dimension)
+    {
+        float dist = distanceBetween(points, centroid, dimension);
+        if (dist < bestDist)
+        {
+            bestCluster = c;
+            bestDist = dist;
+        }
+    }
+    return bestCluster;
+}
+
+// Add points into a sum of points.
+void addPoint(const float *points, float *sums, const int dimension)
+{
+    for (int i = 0; i < dimension; i++)
+    {
+        sums[i] += points[i];
+    }
+}
+
+// Print the centroids one per line.
+void notifyUpdateCentroids(float *centroids, const int k, const int dimension, int *counter)
+{
+    FILE *fpo;
+    if (*counter == 1)
+    {
+        fpo = fopen("./data/old_centroids.dat", "w");
+    }
+    else
+    {
+
+        fpo = fopen("./data/old_centroids.dat", "a");
+    }
+
+    float *p = centroids;
+
+    for (int i = 0; i < k; i++)
+    {
+        printf("Update centroids: ");
+        for (int j = 0; j < dimension; j++, p++)
+        {
+            printf("%f ", *p);
+            fprintf(fpo, "%f\t", *p);
+        }
+        fprintf(fpo, "\n");
+        printf("\n");
+    }
+
+    fclose(fpo);
+
+    *counter = *counter + 1;
+}
+
+void inittialCentroids(float *centroids, const int k, const int dimension)
+{
+    FILE *fpo = fopen("./data/init_centroids.dat", "w");
+
+    float *p = centroids;
+
+    for (int i = 0; i < k; i++)
+    {
+        printf("Initital centroids: ");
+        for (int j = 0; j < dimension; j++, p++)
+        {
+            printf("%f ", *p);
+            fprintf(fpo, "%f\t", *p);
+        }
+        // fprintf(fpo, "\n");
+        printf("\n");
+    }
+
+    fclose(fpo);
+}
+
+// Creates an array of random floats. Each number has a value from 0 - 1
+float *createRandomNums(const int totalPoints)
+{
+    float *rand_nums = (float *)malloc(sizeof(float) * totalPoints);
+    // rand_nums != NULL;
+    for (int i = 0; i < totalPoints; i++)
+    {
+        rand_nums[i] = (rand() / (float)RAND_MAX);
+
+    }
+    return rand_nums;
+}
 
 #define MAX_ITERATIONS 10000
 
